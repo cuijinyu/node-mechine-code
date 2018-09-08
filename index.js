f888c7bc4b056c20c7398b4a56026f21544ff68f
崔晋瑜 <2510271615@qq.com>
2018-09-08 17:06:24 +0800

    windows平台获取唯一机器码成功

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const os = require('os');
 const osType = os.type();
+const crypto = require('crypto');
+
+const secret = "nmc";
+
 function getHardWareInfo() {
     switch (osType) {
 
@@ -8,8 +12,7 @@ function getHardWareInfo() {
          **/
         case 'Windows_NT':
             let getHardWare = require('./build/Release/node_windows.node');
-            let infos = getHardWare.hello();
-            console.log(infos);
+            let infos = getHardWare.exports();
             return infos;
             break;
 
@@ -27,4 +30,16 @@ function getHardWareInfo() {
     }
 }
 
-getHardWareInfo();
+function hash(text) {
+    let hashResult = crypto.createHmac('sha256', secret).update(text).digest('hex');
+    return hashResult;
+}
+
+module.exports = () => {
+    let info = getHardWareInfo();
+    let str = `${info.baseBoardSerialNumber}${info.processorId}${info.macAddress}`;
+    console.log(str);
+    let res = hash(str);
+    console.log(res);
+    return res;
+}
--- src/node_windows.cc
+++ src/node_windows.cc
@@ -15,12 +15,19 @@ namespace demo {
 	void Method(const FunctionCallbackInfo<Value>& args) {
 		Isolate * isolate = args.GetIsolate();
 		map<const char *, char *> infos = getAllInfo();
-		const char * macAddressRoot = infos["macAddressRoot"];
-		args.GetReturnValue().Set(String::NewFromUtf8(isolate, macAddressRoot));
+		Local<Object> obj = Object::New(isolate);
+		obj->Set(String::NewFromUtf8(isolate, "macAddressRoot"), String::NewFromUtf8(isolate, infos["macAddressRoot"]));
+		obj->Set(String::NewFromUtf8(isolate, "diskDriveSerialNumber"), String::NewFromUtf8(isolate, infos["diskDriveSerialNumber"]));
+		obj->Set(String::NewFromUtf8(isolate, "baseBoardSerialNumber"), String::NewFromUtf8(isolate, infos["baseBoardSerialNumber"]));
+		obj->Set(String::NewFromUtf8(isolate, "processorId"), String::NewFromUtf8(isolate, infos["processorId"]));
+		obj->Set(String::NewFromUtf8(isolate, "biosSerialNumber"), String::NewFromUtf8(isolate, infos["biosSerialNumber"]));
+		obj->Set(String::NewFromUtf8(isolate, "baseBoard"), String::NewFromUtf8(isolate, infos["baseBoard"]));
+		obj->Set(String::NewFromUtf8(isolate, "macAddress"), String::NewFromUtf8(isolate, infos["macAddress"]));
+		args.GetReturnValue().Set(obj);
 	}
 
 	void init(Local<Object> exports) {
-		NODE_SET_METHOD(exports, "hello", Method);
+		NODE_SET_METHOD(exports, "exports", Method);
 	}
 
 	NODE_MODULE(addon, init);
